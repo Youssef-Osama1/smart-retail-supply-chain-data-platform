a696e25bd2a55298a4c9bbac9a79f02e420ac3e3
Youssef Osama <youssefosama20055@gmail.com>
2026-03-10 17:38:11 +0200

    implement silver layer pipeline

---
+++ src/pipelines/run_silver_pipeline.py
@@ -0,0 +1,28 @@
+from src.Silver.clean_products import clean_products
+from src.Silver.clean_customers import clean_customers
+from src.Silver.clean_stores import clean_stores
+from src.Silver.clean_warehouses import clean_warehouses
+from src.Silver.clean_orders import clean_orders
+from src.Silver.clean_order_items import clean_order_items
+from src.Silver.clean_store_inventory import clean_store_inventory
+from src.Silver.clean_warehouse_inventory import clean_warehouse_inventory
+from src.Silver.clean_shipments import clean_shipments
+from src.Silver.clean_shipment_items import clean_shipment_items
+
+
+def run_silver_pipeline():
+    clean_products()
+    clean_customers()
+    clean_stores()
+    clean_warehouses()
+    clean_orders()
+    clean_order_items()
+    clean_store_inventory()
+    clean_warehouse_inventory()
+    clean_shipments()
+    clean_shipment_items()
+    print("\nSilver Pipeline Completed Successfully")
+
+
+if __name__ == "__main__":
+    run_silver_pipeline()
